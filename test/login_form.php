8befba78039686598c1637b152f2fe32b9f3679c
REF <127282055+AIMI0808@users.noreply.github.com>
2023-09-22 11:11:16 +0900

    Create login_form.php

---
+++ test/login_form.php
@@ -0,0 +1,16 @@
+<h1>ログインページ</h1>
+<form action="login.php" method="post">
+<div>
+    <label>
+        メールアドレス：
+        <input type="text" name="mail" required>
+    </label>
+</div>
+<div>
+    <label>
+        パスワード：
+        <input type="password" name="pass" required>
+    </label>
+</div>
+<input type="submit" value="ログイン">
+</form>
