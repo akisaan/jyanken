8cd7799dc50761be181050b6e64fd9ef5e2c924b
REF <127282055+AIMI0808@users.noreply.github.com>
2023-09-22 11:11:10 +0900

    Create index.php

---
+++ test/index.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+$username = $_SESSION['name'];
+if (isset($_SESSION['id'])) {//ログインしているとき
+    $msg = 'こんにちは' . htmlspecialchars($username, \ENT_QUOTES, 'UTF-8') . 'さん';
+    $link = '<a href="logout.php">ログアウト</a>';
+} else {//ログインしていない時
+    $msg = 'ログインしていません';
+    $link = '<a href="login.php">ログイン</a>';
+}
+?>
+<h1><?php echo $msg; ?></h1>
+<?php echo $link; ?>
