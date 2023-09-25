e7d1d583bfce31b5827bb277891728b547197528
matakeayumu <matakeayumu@au.com>
2023-09-25 11:56:38 +0900

    Create server_connection.php

---
+++ aaa/chat/server_connection.php
@@ -0,0 +1,13 @@
+<?php error_reporting(0); ?>
+<?php
+$pdo=new PDO(
+  'mysql:host=localhost;dbname=kadai;charset=utf8',
+  'staff',
+  'password'
+);
+// $pdo=new PDO(
+//   'mysql:host=mysql215.phy.lolipop.lan;dbname=LAA1530773-cafe22teamd;charset=utf8',
+//   'LAA1530773',
+//   'Fic20222023It'
+// );
+?>
