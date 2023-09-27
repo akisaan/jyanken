d1d794c5fe2dc6df56cd35c95c16540f415b8890
Toki_Toki <127282055+AIMI0808@users.noreply.github.com>
2023-09-27 09:48:39 +0900

    Create sinki.php

---
+++ aaa/chat/sinki.php
@@ -0,0 +1,8 @@
+<form method="post" action="login.php">
+<label>プレイヤー名:</label>
+<input type="text" name="username">
+<label>パスワード:</label>
+<input type="password" name="password">
+<input type="submit" value="ログイン">
+</form>
+<a href="register.php">新規登録はこちら</a>
